67db9a4e1cc4e8c65de3d7429ced005bb35246ad
ropa99 <rpahlin@hotmail.com>
2024-06-06 18:10:01 +0200

    Initial commit

---
+++ src/main/java/se/jpalombok/jpalombokworkshop/JpaLombokWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.jpalombok.jpalombokworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLombokWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaLombokWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jpalombok/jpalombokworkshop/JpaLombokWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jpalombok.jpalombokworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLombokWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
